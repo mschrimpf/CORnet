9e05cc80372ce99de8ddf267c4d8a7ded2585f3f
Martin Schrimpf <m4rtinsch@gmail.com>
2018-09-17 15:52:37 -0400

    concatenate input and features at block output

--- cornet_d.py
+++ cornet_d.py
@@ -1,22 +1,31 @@
 import math
 from collections import OrderedDict
+
+import torch
 from torch import nn
 
 
 class Flatten(nn.Module):
-
     def forward(self, x):
         return x.view(x.size(0), -1)
 
 
 class Identity(nn.Module):
-
     def forward(self, x):
         return x
 
 
-class CORblock_S(nn.Module):
+class Transition(nn.Sequential):  # copied from torchvision.models.densenet._Transition
+    def __init__(self, in_channels, out_channels):
+        super(Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(in_channels))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(in_channels, out_channels,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
 
+
+class CORblock_D(nn.Module):
     scale = 4
 
     def __init__(self, in_channels, out_channels, times=1):
@@ -34,7 +43,7 @@ class CORblock_S(nn.Module):
         self.nonlin1 = nn.ReLU()
 
         self.conv2 = nn.Conv2d(out_channels * self.scale, out_channels * self.scale,
-                               kernel_size=3, stride=2, padding=1, bias=False)
+                               kernel_size=3, stride=1, padding=1, bias=False)
         self.nonlin2 = nn.ReLU()
 
         self.conv3 = nn.Conv2d(out_channels * self.scale, out_channels,
@@ -43,6 +52,8 @@ class CORblock_S(nn.Module):
 
         self.output = Identity()
 
+        self.transition = Transition(in_channels=in_channels * 4, out_channels=out_channels)
+
         for t in range(self.times):
             setattr(self, f'norm1_{t}', nn.BatchNorm2d(out_channels * self.scale))
             setattr(self, f'norm2_{t}', nn.BatchNorm2d(out_channels * self.scale))
@@ -50,14 +61,10 @@ class CORblock_S(nn.Module):
 
     def forward(self, inp):
         x = self.conv_input(inp)
+        original_input = x
 
         for t in range(self.times):
-            if t == 0:
-                skip = self.norm_skip(self.skip(x))
-                self.conv2.stride = (2, 2)
-            else:
-                skip = x
-                self.conv2.stride = (1, 1)
+            skip = x
 
             x = self.conv1(x)
             x = getattr(self, f'norm1_{t}')(x)
@@ -74,26 +81,29 @@ class CORblock_S(nn.Module):
             x = self.nonlin3(x)
             output = self.output(x)
 
+        # for DenseNet-169, torch.cat([x, new_features], 1) concatenates (64, 64, 56, 56) and (64, 32, 56, 56)
+        output = torch.cat([original_input, output], 1)
+        output = self.transition(output)
         return output
 
 
-def CORnet_S():
+def CORnet_D():
     model = nn.Sequential(OrderedDict([
         ('V1', nn.Sequential(OrderedDict([
             ('conv1', nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                            bias=False)),
+                                bias=False)),
             ('norm1', nn.BatchNorm2d(64)),
             ('nonlin1', nn.ReLU()),
             ('pool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
             ('conv2', nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1,
-                            bias=False)),
+                                bias=False)),
             ('norm2', nn.BatchNorm2d(64)),
             ('nonlin2', nn.ReLU()),
             ('output', Identity())
         ]))),
-        ('V2', CORblock_S(64, 128, times=2)),
-        ('V4', CORblock_S(128, 256, times=4)),
-        ('IT', CORblock_S(256, 512, times=2)),
+        ('V2', CORblock_D(64, 128, times=2)),
+        ('V4', CORblock_D(128, 256, times=4)),
+        ('IT', CORblock_D(256, 512, times=2)),
         ('decoder', nn.Sequential(OrderedDict([
             ('avgpool', nn.AdaptiveAvgPool2d(1)),
             ('flatten', Flatten()),
